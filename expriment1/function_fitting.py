d740bd4a842570f2920399253b63a862ffe0f224
microhhh <13654840887@163.com>
2019-04-14 23:19:17 +0800

    update

--- expriment1/function_fitting.py
+++ expriment1/function_fitting.py
@@ -6,47 +6,59 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
+# %matplotlib inline
+
+n = 1000
 # 定义三维数据
-x = np.linspace(-3, 3, 1000)
-y = np.linspace(-3, 3, 1000)
+x = np.linspace(-3, 3, n)
+y = np.linspace(-3, 3, n)
 
 X, Y = np.meshgrid(x, y)
 
-pair = np.empty(shape=[0, 2], dtype=float)
+pair = np.zeros(shape=[n * n, 2], dtype=float)
 
-# for i in range(len(x)):
-#     for j in range(len(y)):
-#         pair = np.append(pair, [[x[i], y[j]]], axis=0)
+k = 0
+for i in range(len(x)):
+    for j in range(len(y)):
+        pair[k, 0] = x[i]
+        pair[k, 1] = y[j]
+        k += 1
+
 # print(pair)
+print(len(pair))
 
 Z = 10 / ((4 * np.square(X) - 2.1 * np.power(X, 4) + (1 / 3) * np.power(X, 6) +
            X * Y - 4 * np.square(Y) + 4 * np.power(Y, 4) + 2) * 11)
+# print(X)
+# print(Y)
 # print(Z)
 
-# label = np.empty(shape=[0, 1], dtype=float)
-# for i in range(len(x)):
-#     for j in range(len(y)):
-#         label = np.append(label, [[Z[j, i]]], axis=0)
+label = np.zeros(shape=[n * n, 1], dtype=float)
+k = 0
+for i in range(len(x)):
+    for j in range(len(y)):
+        label[k, 0] = Z[j, i]
+        k += 1
 
 # print(label)
+print(len(label))
 
-# 定义坐标轴
-fig = plt.figure()
-ax3 = plt.axes(projection='3d')
 
-# 作图
-surf = ax3.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='jet')
-# ax3.contour(X,Y,Z, zdim='z',offset=-2，cmap='rainbow)   #等高线图，要设置offset，为Z的最小值
-fig.colorbar(surf, shrink=0.5, aspect=5)
-plt.show()
+# 绘图
+# fig = plt.figure()
+# ax3 = plt.axes(projection='3d')
+
+# surf = ax3.plot_surface(X, Y, Z, cstride=1, cmap='jet')
+# plt.contour(X, Y, Z, 90, cmap='rainbow')  # 等高线图，要设置offset，为Z的最小值
+# fig.colorbar(surf)
+# plt.show()
 
 
 class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-
-        self.layer = nn.Sequential(nn.Linear(2, 6), nn.ReLU(), nn.Linear(6, 1), nn.Sigmoid())
+        self.layer = nn.Sequential(nn.Linear(2, 7), nn.Sigmoid(), nn.Linear(7, 1))
 
         for m in self.layer.modules():
             if isinstance(m, nn.Linear):
@@ -57,18 +69,57 @@ class Net(nn.Module):
         return x
 
 
-pair_t = torch.from_numpy(pair).float()
-label_t = torch.from_numpy(label).float()
-net = Net()
+pair_t = torch.from_numpy(pair).float().cuda()
+label_t = torch.from_numpy(label).float().cuda()
+net = Net().cuda()
 
-criterion = nn.BCELoss()
-optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0.9, nesterov=True)
+# print(pair_t)
+# print(label_t)
 
-# in your training loop:
-# for i in range(6):
-#     for j in range(10000):
+criterion = nn.MSELoss()
+optimizer = optim.SGD(net.parameters(), lr=0.3, momentum=0.9, nesterov=True)
+# optimizer = optim.Adam(net.parameters(), lr=0.03)
+
+# batch_size = 1000
+# for i in range(100):
+#     for j in range(0, n * n, batch_size):
 #         optimizer.zero_grad()
-#         out = net(pair_t[j])
-#         loss = criterion(out, label_t[j])
+#         out = net(pair_t[j:j + batch_size])
+#         loss = criterion(out, label_t[j:j + batch_size])
 #         loss.backward()
 #         optimizer.step()
+#     print(loss.item())
+
+for i in range(5000):
+    optimizer.zero_grad()
+    out = net(pair_t)
+    # print(out)
+    loss = criterion(out, label_t)
+    loss.backward()
+    optimizer.step()
+    print(loss.item())
+
+net.eval()
+out = net(pair_t)
+
+z_out = out.cpu().detach().numpy()
+print(z_out.shape)
+
+Z_out = np.zeros(shape=[n, n], dtype=float)
+k = 0
+for i in range(len(x)):
+    for j in range(len(y)):
+        Z_out[j, i] = z_out[k, 0]
+        k += 1
+
+print(list(net.named_parameters()))
+
+# 绘图
+fig = plt.figure()
+ax3 = plt.axes(projection='3d')
+
+surf = ax3.plot_surface(X, Y, Z_out, cstride=1, cmap='jet')
+# plt.contour(X, Y, Z, 90, cmap='rainbow')  # 等高线图，要设置offset，为Z的最小值
+fig.colorbar(surf)
+plt.show()
+
